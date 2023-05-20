c527bef4f560d08ee6df75e43fc72d5aa8673e29
stelioskarydakis <mrkarydakis@gmail.com>
2023-05-20 11:34:02 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import dotenv from "dotenv";
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import mongoose from "mongoose";
+import router from "./router";
+
+dotenv.config();
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+  console.log("Server running on http://localhost:8080/");
+});
+
+const mongoUrl = process.env.MONGO_URL;
+
+mongoose.Promise = Promise;
+mongoose.connect(mongoUrl);
+mongoose.connection.on("error", (error: Error) => console.log(error));
+
+app.use("/", router());
